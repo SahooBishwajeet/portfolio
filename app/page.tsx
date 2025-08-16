c1f3acaf561978e757062ab0b8fc3e61da25c0c6
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-08-16 23:06:41 +0530

    [SB] [Add] implement FloatingNav component and navItems data

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { navItems } from "@/data";
+
 import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNavbar";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
       </div>
     </main>
